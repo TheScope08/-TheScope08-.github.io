6405aafab2da95bc335a3360fb67b61319941f6b
TheScope08 <gab.scopelliti@gmail.com>
2026-02-13 22:51:14 +0100

    Add secret configuration and messages to config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// ========== CONFIGURAZIONE SEGRETA ==========
+const SECRET_NAME = 'sofia pozzobon';
+const SECRET_DATES = [
+    '7 ottobre 2023',
+    '07/10/2023',
+    '7/10/2023',
+    '2023-10-07',
+    '07-10-2023',
+    '7 ottobre 23',
+    '7 ott 2023'
+];
+
+const HEART_EMOJI = '💝';
+
+const NO_MESSAGES = [
+    "ehm... hai sbagliato? 😅",
+    "Ne sei sicura??",
+    "Non vuoi ripensarci??",
+    "Sicura sicura sicura??",
+    "Ultima occasione per dirmi di sì"
+];
